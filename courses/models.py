cdedefd11f4a5c202bac32c4ce0af321cde4cda2
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-09-20 19:55:15 +0300

    добавил гитигнор

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    preview = models.FileField()
