4f462613b2789f8e9766566c61cadc36e82db867
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-09-20 20:33:22 +0300

    add user

--- courses/models.py
+++ courses/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 class Course(models.Model):
     title = models.CharField(max_length=150, verbose_name='название')
-    preview = models.ImageField(upload_to='courses/', blanck=True, null=True, verbose_name='превью')
+    preview = models.ImageField(upload_to='courses/', blank=True, null=True, verbose_name='превью')
     description = models.TextField(verbose_name='описание')
 
 
 class Lesson(models.Model):
     title = models.CharField(max_length=150, verbose_name='название')
     description = models.TextField(verbose_name='описание')
-    preview = models.ImageField(upload_to='courses/', blanck=True, null=True, verbose_name='превью')
+    preview = models.ImageField(upload_to='courses/', blank=True, null=True, verbose_name='превью')
     link = models.URLField(blank=True, null=True)
--- users/models.py
+++ users/models.py
@@ -1,5 +1,13 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(unique=True, verbose_name='email')
+
+    phone = models.CharField(max_length=20, blank=True, null=True, verbose_name='телефон')
+    city = models.CharField(max_length=100, blank=True, null=True, verbose_name='город')
+    avatar = models.ImageField(upload_to='users/avatars/', blank=True, null=True, verbose_name='аватар')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
