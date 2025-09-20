f18a515c33d0b4fb7051d8999288eb286fbe1cba
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-09-20 20:07:56 +0300

    task2 - models of courses and lessons

--- courses/models.py
+++ courses/models.py
@@ -2,4 +2,12 @@ from django.db import models
 
 class Course(models.Model):
     title = models.CharField(max_length=150, verbose_name='название')
-    preview = models.FileField()
+    preview = models.ImageField(upload_to='courses/', blanck=True, null=True, verbose_name='превью')
+    description = models.TextField(verbose_name='описание')
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=150, verbose_name='название')
+    description = models.TextField(verbose_name='описание')
+    preview = models.ImageField(upload_to='courses/', blanck=True, null=True, verbose_name='превью')
+    link = models.URLField(blank=True, null=True)
