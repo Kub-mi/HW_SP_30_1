75514f871b7da46f8a60b2244fdd5a82f0bc328f
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-09-20 20:42:39 +0300

    соединил курс и урок

--- courses/models.py
+++ courses/models.py
@@ -5,9 +5,19 @@ class Course(models.Model):
     preview = models.ImageField(upload_to='courses/', blank=True, null=True, verbose_name='превью')
     description = models.TextField(verbose_name='описание')
 
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
+
 
 class Lesson(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name='lessons')
     title = models.CharField(max_length=150, verbose_name='название')
     description = models.TextField(verbose_name='описание')
     preview = models.ImageField(upload_to='courses/', blank=True, null=True, verbose_name='превью')
     link = models.URLField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'урок'
+        verbose_name_plural = 'уроки'
+
