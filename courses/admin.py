e501813dff0464f0bb270a2890d61c8928f52d3a
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-09-26 19:24:30 +0300

    задание 2

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Course, Lesson
 
-# Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("id", "title")
+    search_fields = ("title",)  # важно для autocomplete_fields
+
+@admin.register(Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "course")
+    search_fields = ("title",)  # важно для autocomplete_fields
+    autocomplete_fields = ("course",)  # опционально, чтобы в уроке искать курс
--- users/models.py
+++ users/models.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 from django.db import models
+from django.db.models import Q
+
+from courses.models import Course, Lesson
 
 
 class User(AbstractUser):
@@ -11,3 +15,35 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
+
+
+class Payment(models.Model):
+    class Method(models.TextChoices):
+        CASH = 'cash', 'Наличные'
+        TRANSFER = 'transfer', 'Перевод на счет'
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='payments', verbose_name='Пользователь')
+    paid_at = models.DateTimeField(auto_now=True, verbose_name='Дата оплаты')
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True, blank=True, related_name='payments', verbose_name='Оплаченный курс')
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, null=True, blank=True, related_name="payments")
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Сумма оплаты')
+    method = models.CharField(max_length=20, choices=Method.choices, verbose_name="Способ оплаты",)
+
+    class Meta:
+        verbose_name = "Платёж"
+        verbose_name_plural = "Платежи"
+        constraints = [
+            models.CheckConstraint(
+                name="payment_exactly_one_of_course_or_lesson",
+                check=(
+                        (Q(course__isnull=False) & Q(lesson__isnull=True)) |
+                        (Q(course__isnull=True) & Q(lesson__isnull=False))
+                ),
+            )
+        ]
+        ordering = ["-paid_at", "-id"]
+
+    def __str__(self) -> str:
+        target = self.course or self.lesson
+        target_name = getattr(target, "title", str(target)) if target else "—"
+        return f"#{self.pk} {self.user} → {target_name} ({self.amount})"
