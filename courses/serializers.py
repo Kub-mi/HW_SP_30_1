89e9a9ab8efd57899dae088352a91dd8ad8788e5
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-09-26 19:30:15 +0300

    исправил

--- courses/serializers.py
+++ courses/serializers.py
@@ -4,12 +4,19 @@ from courses.models import Course, Lesson
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
+
+    def get_lessons_count(self, obj):
+        if hasattr(obj, "lesson_count"):
+            return obj.lesson_count
+        return obj.lessons.count()
+
     class Meta:
         model = Course
-        fields = '__all__'
+        fields = ('id', 'title', 'preview', 'description', 'lessons_count')
 
 
-class LessonSerializer(serializers.ModelSerializer):
+class LessonShortSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
