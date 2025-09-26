665c87fffa4238584dd7d9e54d7f9830fcc3d547
Михаил <mihailkubrak@MacBook-Air-Mihail.local>
2025-09-26 16:16:01 +0300

    добавил сериалайзеры

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from courses.models import Course, Lesson
 from users.models import User
 
 
@@ -7,3 +8,22 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    lessons_count = serializers.SerializerMethodField()
+
+    def get_lessons_count(self, obj):
+        if hasattr(obj, "lesson_count"):
+            return obj.lesson_count
+        return obj.lessons.count()
+
+    class Meta:
+        model = Course
+        fields = ('id', 'title', 'preview', 'description', 'lessons_count')
+
+
+class LessonSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Lesson
+        fields = "__all__"
